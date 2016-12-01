82004c630ceda4863a1b410fa843722a20ad9e45
pziideck <diegog1991@me.com>
2016-12-01 12:12:11 -0600

    Materialize listo - front terminado

--- internals/handlers.js
+++ internals/handlers.js
@@ -2,13 +2,11 @@ var colors = require("colors");
 colors.setTheme(require("../config/config").colorTheme);
 var fortune = require("./fortune");
 
-// Creando el handler
-// getfortune
+// Creando el handler getfortune
 var _crackTheCookie = function(req, res){
     console.log("> Cookie crash requested...".data);
     fortune.getFortune(function(fortunePaperObj){
-        // Preparando encabezados para contestar
-        // Json
+        // Preparando encabezados para contestar Json
         res.writeHead(200,{
             "Content-Type" : "application/json"
         });
@@ -19,7 +17,7 @@ var _crackTheCookie = function(req, res){
 
 var _getAuthor = function(req, res){
     console.log("> Se solicito: Author..".data);
-    res.end("Author: Ivan Rivalcoba");
+    res.end("Author: Diego Galindo");
 };
 
 
@@ -27,11 +25,9 @@ var _getAuthor = function(req, res){
 var handlers = {};
 
 // Registrando manejadores
-<<<<<<< HEAD
+
 handlers["/getthecookie"] = _crackTheCookie;
-=======
-handlers["/crackthecookie"] = _crackTheCookie;
->>>>>>> dba29a1e4293ee90cd34a0d984526551c1f8ec1d
+
 handlers["/getAuthor"] = _getAuthor;
 
 // Exportando objeto manejador
--- static/js/site.js
+++ static/js/site.js
@@ -1,19 +1,15 @@
 
-<<<<<<< HEAD
         function saludar(){
             swal("Hola chaca...");
         }
 
-        function getFortuneFromServer(){
+        function getFortuneFromServer()
+        {
             //Realizando una peticion get asincrona con ajax asistida con jquery
-            $.get(url,"/getthecookie","",function(cookie, status){
+            $.get("/getthecookie","",function(cookie, status){
                 //Contenido del callback
-                console.log('>status'+status);
+                console.log('> status'+status);
                 //Presentando el mensaje
                 swal(cookie.paper);
-            },'json')
-=======
-        function saludar (){
-            swal("Hola chaca...");
->>>>>>> dba29a1e4293ee90cd34a0d984526551c1f8ec1d
-        }
+             },'json');
+       }
